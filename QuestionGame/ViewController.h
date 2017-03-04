d0ca18675ea1dfb432f618ec7d2747edda90a22a
Elsa Filippidou <elsa.filippidou@gmail.com>
2017-03-04 16:15:39 +0100

    Initial Commit

---
+++ QuestionGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuestionGame
+//
+//  Created by Elsa Filippidou on 04/03/2017.
+//  Copyright © 2017 allilo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QuestionGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QuestionGame
+//
+//  Created by Elsa Filippidou on 04/03/2017.
+//  Copyright © 2017 allilo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
